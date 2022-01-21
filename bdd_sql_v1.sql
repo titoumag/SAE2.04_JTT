72895b9888040313f15ef17f52f2b8282e61a7cc
titoumag <81028684+titoumag@users.noreply.github.com>
2022-01-21 09:54:12 +0100

    MAJ script sql

--- bdd_sql_v1.sql
+++ bdd_sql_v1.sql
@@ -1,138 +1,133 @@
--- drop
-drop table if exists Ligne;
-drop table if exists Commande;
-drop table if exists Client;
-drop table if exists Article;
+DROP TABLE IF EXISTS ligne_commande;
+DROP TABLE IF EXISTS panier;
+DROP TABLE IF EXISTS commande;
+DROP TABLE IF EXISTS casque;
+DROP TABLE IF EXISTS commande;
+DROP TABLE IF EXISTS taille;
+DROP TABLE IF EXISTS fabricant;
+DROP TABLE IF EXISTS couleur;
+DROP TABLE IF EXISTS type_casque;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS etat;
 
--- create
-create table if not exists Article(
-    idArticle int auto_increment,
-    designation varchar(50),
-    prix numeric(5,2),
-
-    primary key (idArticle)
+CREATE TABLE IF NOT EXISTS etat(
+    id int auto_increment,
+    libelle varchar(20),
+    PRIMARY KEY(id)
 )character set 'utf8';
 
-create table if not exists Client(
-    idClient int auto_increment,
-    nom varchar(50),
-    ville varchar(50),
-
-    primary key (idClient)
+CREATE TABLE IF NOT EXISTS user(
+    id int auto_increment,
+    username varchar(255),
+    password varchar(255),
+    role varchar(50),
+    est_actif tinyint(1),
+    pseudo varchar(255),
+    email varchar(255),
+    PRIMARY KEY(id)
 )character set 'utf8';
 
-create table if not exists Commande(
-    idCommande int auto_increment,
-    dateCommande date,
-    idClient int,
+CREATE TABLE IF NOT EXISTS commande(
+    id int auto_increment,
+    date_achat DATE,
+    user_id int,
+    etat_id int,
 
-    primary key (idCommande),
-    constraint fk_commande_client
-        foreign key (idClient) references Client(idClient)
+    CONSTRAINT fk_commande_user
+        FOREIGN KEY(user_id) REFERENCES user(id),
+    CONSTRAINT fk_commande_etat
+        FOREIGN KEY(etat_id) REFERENCES etat(id),
+    PRIMARY KEY(id)
 )character set 'utf8';
 
-create table if not exists Ligne(
-    idCommande int,
-    idArticle int,
-    quantite int,
-
-    primary key (idCommande,idArticle),
-    constraint fk_ligne_commande
-        foreign key (idCommande) references Commande(idCommande),
-    constraint fk_ligne_article
-        foreign key (idArticle) references Article(idArticle)
+CREATE TABLE IF NOT EXISTS type_casque(
+   id int auto_increment,
+   libellle VARCHAR(50),
+   PRIMARY KEY(id)
 )character set 'utf8';
 
--- insert / load
-LOAD DATA LOCAL INFILE 'data/Article.csv' INTO TABLE Article CHARACTER SET utf8
-FIELDS TERMINATED BY ',';
-LOAD DATA LOCAL INFILE 'data/Client.csv' INTO TABLE Client CHARACTER SET utf8
-FIELDS TERMINATED BY ',';
-LOAD DATA LOCAL INFILE 'data/Commande.csv' INTO TABLE Commande CHARACTER SET utf8
-FIELDS TERMINATED BY ',';
-LOAD DATA LOCAL INFILE 'data/Ligne.csv' INTO TABLE Ligne CHARACTER SET utf8
-FIELDS TERMINATED BY ',';
+CREATE TABLE IF NOT EXISTS taille(
+   id int auto_increment,
+   libelle VARCHAR(50),
+   PRIMARY KEY(id)
+)character set 'utf8';
 
-alter table Commande drop foreign key fk_commande_client;
-alter table Ligne drop foreign key fk_ligne_article;
-alter table Ligne drop foreign key fk_ligne_commande;
+CREATE TABLE IF NOT EXISTS fabricant(
+   id int auto_increment,
+   nom VARCHAR(50),
+   adresse VARCHAR(50),
+   PRIMARY KEY(id)
+)character set 'utf8';
 
-alter table Commande
-    add constraint fk_commande_client
-        foreign key (idClient) references Client(idClient) on delete cascade;
-alter table Ligne
-    add constraint fk_ligne_commande
-        foreign key (idCommande) references Commande(idCommande) on delete cascade;
-alter table Ligne
-    add constraint fk_ligne_article
-        foreign key (idArticle) references Article(idArticle) on delete cascade;
+CREATE TABLE IF NOT EXISTS couleur(
+   id int auto_increment,
+   libelle VARCHAR(50),
+   PRIMARY KEY(id)
+)character set 'utf8';
 
-show create table Commande;
-show create table Ligne;
+CREATE TABLE IF NOT EXISTS commande(
+   id int auto_increment,
+   date_achat DATE,
+   user_id INT NOT NULL,
+   etat_id INT NOT NULL,
 
-delete from Client where nom like 'Mutz';
-select * from Client;
-select * from Commande;
+   CONSTRAINT fk_commande_etat
+       FOREIGN KEY(etat_id) REFERENCES etat(id),
+   CONSTRAINT fk_commande_user
+       FOREIGN KEY(user_id) REFERENCES user(id),
+   PRIMARY KEY(id)
+)character set 'utf8';
 
--- r1
-select nom
-from Client
-where ville='Belfort'
-  and (nom like 'm%' or nom like 'e%' or nom like 'd%')
-order by nom;
+CREATE TABLE IF NOT EXISTS casque(
+   id int auto_increment,
+   N_serie INT,
+   fabricant_id INT NOT NULL,
+   taille_id INT NOT NULL,
+   couleur_id INT NOT NULL,
+   type_casque_id INT NOT NULL,
 
--- r2
-select designation,prix
-from Article
-where prix>=6 and prix<=10 and designation like '%lég%'
-order by designation;
+   CONSTRAINT fk_casque_fabriquant
+       FOREIGN KEY(fabricant_id) REFERENCES fabricant(id),
+   CONSTRAINT fk_casque_taille
+       FOREIGN KEY(taille_id) REFERENCES taille(id),
+   CONSTRAINT fk_casque_couleur
+        FOREIGN KEY(couleur_id) REFERENCES couleur(id),
+   CONSTRAINT fk_casque_type
+       FOREIGN KEY(type_casque_id) REFERENCES type_casque(id),
+   PRIMARY KEY(id)
+)character set 'utf8';
 
--- r3
-select nom,dateCommande
-from Client
-inner join Commande C on Client.idClient = C.idClient
-where nom like '%Mutz%';
+CREATE TABLE IF NOT EXISTS panier(
+   id int auto_increment,
+   date_ajout DATE,
+   prix_unit DECIMAL(15,2),
+   quantite VARCHAR(50),
+   casque_id INT NOT NULL,
+   user_id INT NOT NULL,
 
--- r4
-select nom,designation,prix,quantite,Commande.idCommande
-from Article
-inner join Ligne on Article.idArticle = Ligne.idArticle
-inner join Commande on Ligne.idCommande = Commande.idCommande
-inner join Client on Commande.idClient = Client.idClient
-where nom like '%Mutz%';
+   CONSTRAINT fk_panier_casque
+       FOREIGN KEY(casque_id) REFERENCES casque(id),
+    CONSTRAINT fk_panier_user
+       FOREIGN KEY(user_id) REFERENCES user(id),
+    PRIMARY KEY(id)
+)character set 'utf8';
 
--- r5
-select nom,designation,Commande.idCommande,prix*quantite as ca
-from Article
-inner join Ligne on Article.idArticle = Ligne.idArticle
-inner join Commande on Ligne.idCommande = Commande.idCommande
-inner join Client on Commande.idClient = Client.idClient
-where nom like '%Mutz%'
-order by ca desc;
+CREATE TABLE IF NOT EXISTS ligne_commande(
+   commande_id INT,
+   casque_id INT,
+   prix_unit DECIMAL(6,2),
+   quantite DECIMAL(7,3),
 
--- r6
-select nom,Commande.idCommande,SUM(prix*quantite) as prix_total
-from Article
-inner join Ligne on Article.idArticle = Ligne.idArticle
-inner join Commande on Ligne.idCommande = Commande.idCommande
-inner join Client on Commande.idClient = Client.idClient
-where nom='Mutz'
-group by Commande.idCommande, nom;
+   CONSTRAINT fk_ligne_commande_commande
+       FOREIGN KEY(commande_id) REFERENCES commande(id),
+   CONSTRAINT fk_ligne_commande_casque
+       FOREIGN KEY(casque_id) REFERENCES casque(id),
+   PRIMARY KEY(commande_id, casque_id)
+)character set 'utf8';
 
 
--- r7
-select nom,Commande.idCommande,SUM(prix*quantite) as prix_total,
-       SUM(prix*quantite)/5 as TVA, SUM(prix*quantite)*1.2 as prix_total_TTC
-from Article
-inner join Ligne on Article.idArticle = Ligne.idArticle
-inner join Commande on Ligne.idCommande = Commande.idCommande
-inner join Client on Commande.idClient = Client.idClient
-group by Commande.idCommande, nom
-order by SUM(prix*quantite);
 
--- r8
-select designation,quantite as QteCommande,year(dateCommande),Commande.idCommande
-from Article
-left join  Ligne on Ligne.idArticle = Article.idArticle
-left join Commande on Ligne.idCommande = Commande.idCommande
-order by designation;
+INSERT INTO user (email, username, password, role,  est_actif) VALUES
+('admin@admin.fr', 'admin', 'sha256$pBGlZy6UukyHBFDH$2f089c1d26f2741b68c9218a68bfe2e25dbb069c27868a027dad03bcb3d7f69a', 'ROLE_admin', 1),
+('client@client.fr', 'client', 'sha256$Q1HFT4TKRqnMhlTj$cf3c84ea646430c98d4877769c7c5d2cce1edd10c7eccd2c1f9d6114b74b81c4', 'ROLE_client', 1),
+('client2@client2.fr', 'client2', 'sha256$ayiON3nJITfetaS8$0e039802d6fac2222e264f5a1e2b94b347501d040d71cfa4264cad6067cf5cf3', 'ROLE_client', 1);
