86fa73797658a9c1f8675b9e3bebd92eb368b356
joudot <julien.oudot@laposte.net>
2022-01-25 09:05:41 +0100

    filtre

--- controllers/client_article.py
+++ controllers/client_article.py
@@ -12,12 +12,38 @@ client_article = Blueprint('client_article', __name__,
 @client_article.route('/client/article/show')      # remplace /client
 def client_article_show():                                 # remplace client_index
     mycursor = get_db().cursor()
-    mycursor.execute("SELECT * FROM casque")
+
+    debut = True
+    params = []
+    sql = "SELECT * FROM casque"
+    if "filter_word" in session.keys():
+        sql+=" WHERE libelle LIKE %s"
+        params.append("%"+session['filter_word']+"%")
+        debut = False
+    if "filter_types" in session.keys() and len(session['filter_types'])>0:
+        if len(session['filter_types']) == 1: tpl = "('" + str(session['filter_types'][0]) + "')"
+        else: tpl = str(tuple(session['filter_types']))
+        if debut: sql+=" WHERE type_casque_id in "+tpl
+        else: sql+=" AND type_casque_id in "+tpl
+        debut=False
+    if "filter_prix_min" in session.keys() !="":
+        if debut: sql+=" WHERE prix > %s"
+        else: sql+=" AND prix > %s"
+        debut=False
+        params.append(session['filter_prix_min'])
+    if "filter_prix_max" in session.keys() !="":
+        if debut: sql+=" WHERE prix < %s"
+        else: sql+=" AND prix < %s"
+        params.append(session['filter_prix_max'])
+
+    mycursor.execute(sql,params)
     articles = mycursor.fetchall()
+
+
+
     mycursor.execute("SELECT * FROM type_casque")
     types_articles = mycursor.fetchall()
     articles_panier = []
-    print(types_articles)
     prix_total = None
     return render_template('client/boutique/panier_article.html', articles=articles, articlesPanier=articles_panier, prix_total=prix_total, itemsFiltre=types_articles)
 
