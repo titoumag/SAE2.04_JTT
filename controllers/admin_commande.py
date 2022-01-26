ee1f2310319db9886fbf94e11548d4ef167110d4
Titouan NICOT <titouan.nicot@edu.univ-fcomte.fr>
2022-01-26 13:30:34 +0100

    admin gestion commande + mise a jour bdd

--- controllers/admin_commande.py
+++ controllers/admin_commande.py
@@ -1,28 +1,48 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 from flask import Blueprint
-from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
-
+from flask import render_template, redirect
 from connexion_db import get_db
 
 admin_commande = Blueprint('admin_commande', __name__,
-                        template_folder='templates')
+                           template_folder='templates')
+
 
 @admin_commande.route('/admin/commande/index')
 def admin_index():
     return render_template('admin/layout_admin.html')
 
 
-@admin_commande.route('/admin/commande/show', methods=['get','post'])
+@admin_commande.route('/admin/commande/show', methods=['get', 'post'])
 def admin_commande_show():
     mycursor = get_db().cursor()
-    commandes = []
-    articles_commande = None
-    return render_template('admin/commandes/show.html', commandes=commandes, articles_commande=articles_commande)
+    sql = "SELECT id, user_id, SUM(quantite) as commande_quantite,SUM(quantite*prix_unit) as prix_tot, etat_id, date_achat " \
+          "FROM ligne_commande " \
+          "INNER JOIN commande on commande.id=ligne_commande.commande_id " \
+          "GROUP BY commande.id"
+    mycursor.execute(sql)
+    commandes = mycursor.fetchall()
+
+    return render_template('admin/commandes/show.html', commandes=commandes)
 
 
-@admin_commande.route('/admin/commande/valider', methods=['get','post'])
-def admin_commande_valider():
+@admin_commande.route('/admin/commande/valider/<int:id>', methods=['get', 'post'])
+def admin_commande_valider(id):
     mycursor = get_db().cursor()
+    sql = '''UPDATE commande SET etat_id=2 WHERE commande.id=%s'''
+    mycursor.execute(sql, id)
+    get_db().commit()
+    return redirect('/admin/commande/show')
 
-    return redirect('/admin/commande/show') 
+
+@admin_commande.route('/admin/commande/<int:id>', methods=['get', 'post'])
+def admin_commande_details(id):
+    mycursor = get_db().cursor()
+    sql = '''select commande_id, casque.libelle as libelle, prix_unit, quantite, prix_unit*quantite as prix_tot from
+     ligne_commande INNER JOIN casque on casque.id = casque_id where commande_id = %s'''
+    mycursor.execute(sql, id)
+    articles = mycursor.fetchall()
+    sql = '''select libelle from etat inner join commande on commande.etat_id=etat.id where commande.id = %s '''
+    mycursor.execute(sql, id)
+    etat = mycursor.fetchone()
+    return render_template('admin/commandes/detail/show.html', articles=articles, etat=etat)
--- controllers/client_article.py
+++ controllers/client_article.py
@@ -1,57 +1,68 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 from flask import Blueprint
-from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
+from flask import render_template, session
 
 from connexion_db import get_db
 
 client_article = Blueprint('client_article', __name__,
-                        template_folder='templates')
+                           template_folder='templates')
+
 
 @client_article.route('/client/index')
-@client_article.route('/client/article/show')      # remplace /client
-def client_article_show():                                 # remplace client_index
+@client_article.route('/client/article/show')  # remplace /client
+def client_article_show():  # remplace client_index
     mycursor = get_db().cursor()
 
     debut = True
     params = []
     sql = "SELECT * FROM casque"
     if "filter_word" in session.keys():
-        sql+=" WHERE libelle LIKE %s"
-        params.append("%"+session['filter_word']+"%")
+        sql += " WHERE libelle LIKE %s"
+        params.append("%" + session['filter_word'] + "%")
         debut = False
-    if "filter_types" in session.keys() and len(session['filter_types'])>0:
-        if len(session['filter_types']) == 1: tpl = "('" + str(session['filter_types'][0]) + "')"
-        else: tpl = str(tuple(session['filter_types']))
-        if debut: sql+=" WHERE type_casque_id in "+tpl
-        else: sql+=" AND type_casque_id in "+tpl
-        debut=False
-    if "filter_prix_min" in session.keys() and session["filter_prix_min"] !="":
-        if debut: sql+=" WHERE prix > %s"
-        else: sql+=" AND prix > %s"
-        debut=False
+    if "filter_types" in session.keys() and len(session['filter_types']) > 0:
+        if len(session['filter_types']) == 1:
+            tpl = "('" + str(session['filter_types'][0]) + "')"
+        else:
+            tpl = str(tuple(session['filter_types']))
+        if debut:
+            sql += " WHERE type_casque_id in " + tpl
+        else:
+            sql += " AND type_casque_id in " + tpl
+        debut = False
+    if "filter_prix_min" in session.keys() and session["filter_prix_min"] != "":
+        if debut:
+            sql += " WHERE prix > %s"
+        else:
+            sql += " AND prix > %s"
+        debut = False
         params.append(session['filter_prix_min'])
-    if "filter_prix_max" in session.keys() and session["filter_prix_max"] !="":
-        if debut: sql+=" WHERE prix < %s"
-        else: sql+=" AND prix < %s"
+    if "filter_prix_max" in session.keys() and session["filter_prix_max"] != "":
+        if debut:
+            sql += " WHERE prix < %s"
+        else:
+            sql += " AND prix < %s"
         params.append(session['filter_prix_max'])
 
-    mycursor.execute(sql,params)
+    mycursor.execute(sql, params)
     articles = mycursor.fetchall()
-
-
 
     mycursor.execute("SELECT * FROM type_casque")
     types_articles = mycursor.fetchall()
-    mycursor.execute("SELECT * FROM panier INNER JOIN casque ON panier.casque_id=casque.id WHERE user_id=%s",session['user_id'])
+    mycursor.execute("SELECT * FROM panier INNER JOIN casque ON panier.casque_id=casque.id WHERE user_id=%s",
+                     session['user_id'])
     articles_panier = mycursor.fetchall()
     prix_total = None
-    return render_template('client/boutique/panier_article.html', articles=articles, articlesPanier=articles_panier, prix_total=prix_total, itemsFiltre=types_articles)
+    return render_template('client/boutique/panier_article.html', articles=articles, articlesPanier=articles_panier,
+                           prix_total=prix_total, itemsFiltre=types_articles)
+
 
 @client_article.route('/client/article/details/<int:id>', methods=['GET'])
 def client_article_details(id):
     mycursor = get_db().cursor()
-    article=None
-    commentaires=None
-    commandes_articles=None
-    return render_template('client/boutique/article_details.html', article=article, commentaires=commentaires, commandes_articles=commandes_articles)
+    article = None
+    commentaires = None
+    commandes_articles = None
+    return render_template('client/boutique/article_details.html', article=article, commentaires=commentaires,
+                           commandes_articles=commandes_articles)
