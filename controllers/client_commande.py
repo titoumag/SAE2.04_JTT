d0ad4805573b63d8355d254fed37ba999921b3fb
joudot <julien.oudot@laposte.net>
2022-01-26 12:28:06 +0100

    details commande user

--- controllers/client_commande.py
+++ controllers/client_commande.py
@@ -46,17 +46,31 @@ def client_commande_add():
 
 
 
-@client_commande.route('/client/commande/show', methods=['get','post'])
+@client_commande.route('/client/commande/show', methods=['GET','POST'])
 def client_commande_show():
     mycursor = get_db().cursor()
 
-    sql = "select date_achat, count(*) as nbr_articles,sum(quantite) as nb_tot,sum(prix_unit*quantite) as prix_total,etat_id " \
+    currentCommande = request.form.get("idCommande",'')
+    print("commande : ",currentCommande)
+
+    sql = "select commande.id,libelle,date_achat, count(*) as nbr_articles,sum(quantite) as nb_tot,sum(prix_unit*quantite) as prix_total,etat_id " \
         "from commande " \
         "inner join ligne_commande on commande.id=ligne_commande.commande_id " \
-        "where user_id=%s " \
-        "group by date_achat,etat_id;"#"inner join etat on etat.id=commande.etat_id "
+        "inner join etat on commande.etat_id=etat.id" \
+          " where user_id=%s " \
+          "group by commande.id,date_achat,etat_id;"
     mycursor.execute(sql,session['user_id'])
-    commandes = mycursor.fetchall();
-    articles_commande = None
+    commandes = mycursor.fetchall()
+
+    if currentCommande is not None:
+        sql = "SELECT quantite,prix_unit as prix,casque.libelle as nom,sum(prix_unit*quantite) as prix_ligne " \
+              "FROM ligne_commande " \
+              "INNER JOIN casque ON casque_id = casque.id " \
+              "WHERE commande_id = %s " \
+              "GROUP BY casque.id"
+        mycursor.execute(sql,(currentCommande))
+        articles_commande = mycursor.fetchall()
+    else:
+        articles_commande = None
     return render_template('client/commandes/show.html', commandes=commandes, articles_commande=articles_commande)
 
