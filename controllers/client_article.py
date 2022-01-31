4575fd00f951987e1252dffb09813dd9297b55f1
Julien Oudot <great.caribou@gmail.com>
2022-01-31 09:51:24 +0100

    base pour les avis final

--- controllers/client_article.py
+++ controllers/client_article.py
@@ -16,7 +16,8 @@ def client_article_show():  # remplace client_index
 
     debut = True
     params = []
-    sql = "SELECT id,libelle,image,stock,prix,fabricant_id,taille_id,couleur_id,type_casque_id,sum(avis.user_id) as n_avis,AVG(avis.note) as moy_notes FROM casque LEFT JOIN avis ON casque.id = avis.casque_id"
+    sql = "SELECT id,libelle,image,stock,prix,fabricant_id,taille_id,couleur_id,type_casque_id,count(avis.user_id) as nb_avis,AVG(avis.note) as moy_notes " \
+          "FROM casque LEFT JOIN avis ON casque.id = avis.casque_id"
     if "filter_word" in session.keys():
         sql += " WHERE libelle LIKE %s"
         params.append("%" + session['filter_word'] + "%")
