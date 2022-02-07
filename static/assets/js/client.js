654b4f4c0d15c21dece910dff1c10a5dc7dad1e2
Julien Oudot <great.caribou@gmail.com>
2022-02-07 09:40:53 +0100

    conditions utilisations + logo

--- static/assets/js/client.js
+++ static/assets/js/client.js
@@ -18,12 +18,3 @@ function getContent(sender, objetMail, message, id) {
     from.innerHTML = 'from : ' + sender;
     idDelete.href = idDelete.getAttribute("href") + id;
 }
-
-function add_money(){
-    argent = prompt("combien voulez-vous ajouter ?\n" +
-        " 10 % seront débité en plus pour les frais de transaction");
-    if (isNaN(argent))
-        alert("erreur nombre");
-    else
-        document.location.href="/client/info/add_money/"+argent;
-}
