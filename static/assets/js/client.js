007a77c5404a706e4e1d0f6d138c39afadbf4825
Julien Oudot <great.caribou@gmail.com>
2022-02-07 09:41:59 +0100

    merge

--- static/assets/js/client.js
+++
@@ -1,8 +0,0 @@
-function add_money(){
-    let argent = prompt("combien voulez-vous ajouter ?\n" +
-        " 10 % seront débité en plus pour les frais de transaction");
-    if (isNaN(argent))
-        alert("erreur nombre");
-    else
-        document.location.href="/client/info/add_money/"+argent;
-}
