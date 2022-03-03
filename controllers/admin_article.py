7a9d4f71d3f6ef69c1c543f467d9d4e8fdf07384
tnicot <titouan.nicot@edu.univ-fcomte.fr>
2022-03-03 10:33:45 +0100

    début edit article

--- controllers/admin_article.py
+++ controllers/admin_article.py
@@ -1,7 +1,10 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
+import os.path
+
 from flask import Blueprint
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
+from werkzeug.utils import secure_filename
 
 from connexion_db import get_db
 
@@ -61,24 +64,45 @@ def edit_article(id):
     article = mycursor.fetchall()
     sql = '''select * from type_casque'''
     mycursor.execute(sql)
-    types_articles = mycursor.fetchall()
-    return render_template('admin/article/edit_article.html', article=article, types_articles=types_articles)
+    types_casques = mycursor.fetchall()
+    sql = '''select id, nom from fabricant'''
+    mycursor.execute(sql)
+    fabricants = mycursor.fetchall()
+    sql = '''select * from couleur'''
+    mycursor.execute(sql)
+    couleurs = mycursor.fetchall()
+    print(article)
+    return render_template('admin/article/edit_article.html', article=article,
+                           types_casques=types_casques, fabricants=fabricants,
+                           couleurs=couleurs)
 
 
 @admin_article.route('/admin/article/edit', methods=['POST'])
 def valid_edit_article():
+    mycursor = get_db().cursor()
     nom = request.form['nom']
     id = request.form.get('id', '')
-    type_article_id = request.form.get('type_article_id', '')
-    # type_article_id = int(type_article_id)
+    type_casque_id = request.form.get('type_casque_id', '')
     prix = request.form.get('prix', '')
     stock = request.form.get('stock', '')
-    description = request.form.get('description', '')
-    image = request.form.get('image', '')
+    fabricant = request.form.get('fabricant', '')
+    image = request.files.get('image')
+    filename = secure_filename(image.filename)
+    image.save(os.path.join('static/images/', filename))
+    couleur = request.form.get('couleur', '')
+    # sql = '''update casque set libelle=%s, image=%s, stock=%s, prix=%s, couleur_id=%s, fabricant_id=%s, type_casque_id=%s  where id=%s'''
+    # print(sql)
 
-    print(u'article modifié , nom : ', nom, ' - type_article:', type_article_id, ' - prix:', prix, ' - stock:', stock,
-          ' - description:', description, ' - image:', image)
-    message = u'article modifié , nom:' + nom + '- type_article:' + type_article_id + ' - prix:' + prix + ' - stock:' + stock + ' - description:' + description + ' - image:' + image
+    print(nom, image, stock, prix, couleur, fabricant, type_casque_id, id)
+    # mycursor.execute(sql, (nom, image, stock,
+    #                        prix, couleur, fabricant,
+    #                        type_casque_id, id))
+
+    # get_db().commit()
+
+    print(u'article modifié , nom : ', nom, ' - type_casque :', type_casque_id, ' - prix:', prix, ' - stock:', stock,
+          ' - fabricant:', fabricant, ' - image:', image)
+    message = u'article modifié , nom:' + nom + '- type_casque :' + type_casque_id + ' - prix:' + prix + ' - stock:' + stock + ' - fabricant:' + fabricant + ' - image:' + image
     flash(message)
     return redirect(url_for('admin_article.show_article'))
 
@@ -98,10 +122,10 @@ def reboot():
 @admin_article.route('/admin/article/bilan')
 def dataviz_article():
     mycursor = get_db().cursor()
-    sql="SELECT type_casque_id,type_casque.libelle as libelle,SUM(prix*stock) as prix_total " \
-        "FROM casque " \
-        "INNER JOIN type_casque ON type_casque_id=type_casque.id " \
-        "GROUP BY type_casque_id"
+    sql = "SELECT type_casque_id,type_casque.libelle as libelle,SUM(prix*stock) as prix_total " \
+          "FROM casque " \
+          "INNER JOIN type_casque ON type_casque_id=type_casque.id " \
+          "GROUP BY type_casque_id"
     mycursor.execute(sql)
     casques = mycursor.fetchall()
     lPercentage = []
@@ -110,17 +134,15 @@ def dataviz_article():
 
     maxi = 0.0
     for type in casques:
-        maxi+=float(type["prix_total"])
-
+        maxi += float(type["prix_total"])
 
     for type in casques:
         lTotaux.append(float(type["prix_total"]))
         if maxi == 0.0:
             lPercentage.append(0.0)
         else:
-            lPercentage.append( (float(type["prix_total"])/maxi) * 100)
+            lPercentage.append((float(type["prix_total"]) / maxi) * 100)
         lLibelle.append(type["libelle"])
-
 
     mycursor.execute("SELECT type_casque.libelle as libelle, type_casque.id as id, SUM(stock) as stockTotal,"
                      " SUM(stock*prix) as coutTotal "
@@ -133,16 +155,16 @@ def dataviz_article():
           "from adresse " \
           "group by substring(code,1,2)"
     mycursor.execute(sql)
-    adresse=mycursor.fetchall()
-    max=0
+    adresse = mycursor.fetchall()
+    max = 0
     for element in adresse:
-        if element['nombre']>max:
-            max=element['nombre']
+        if element['nombre'] > max:
+            max = element['nombre']
 
-    lettre="FEDCBA9876543210"
-    couleur = ['#'+lettre[int(i/max*16)-1]*6 for i in range(1,max+1)]
+    lettre = "FEDCBA9876543210"
+    couleur = ['#' + lettre[int(i / max * 16) - 1] * 6 for i in range(1, max + 1)]
     print(couleur)
 
-    return render_template('admin/dataviz/etat_article_vente.html',tableau=tableau, casques=casques,
-                                                        percentages = lPercentage,libelle = lLibelle,totaux=lTotaux,
-                                                    adresse=adresse,couleur=couleur)
+    return render_template('admin/dataviz/etat_article_vente.html', tableau=tableau, casques=casques,
+                           percentages=lPercentage, libelle=lLibelle, totaux=lTotaux,
+                           adresse=adresse, couleur=couleur)
